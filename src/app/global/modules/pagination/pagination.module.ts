201e8815d608fb0c87bca0afb9d171eec950e255
Nikita Kisiura <nikitar156@gmail.com>
2022-04-27 15:23:56 +0300

    created pagination module

---
+++ src/app/global/modules/pagination/pagination.module.ts
@@ -0,0 +1,11 @@
+import {NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {PaginationComponent} from "./components/pagination.component";
+
+@NgModule({
+  declarations: [PaginationComponent],
+  imports: [CommonModule],
+  providers: [],
+  exports: []
+})
+export class PaginationModule { }
