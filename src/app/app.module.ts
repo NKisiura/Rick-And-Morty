e7bd5b1b83857dd8ac69abcd8f4f669fd8a577e6
Nikita Kisiura <nikitar156@gmail.com>
2022-05-01 10:47:30 +0300

    added ngrx store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CharacterListModule } from "./character-list/character-list.module";
 import { PageNotFoundModule } from "./global/modules/page-not-found/page-not-found.module";
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [
@@ -14,7 +18,10 @@ import { PageNotFoundModule } from "./global/modules/page-not-found/page-not-fou
     BrowserModule,
     AppRoutingModule,
     CharacterListModule,
-    PageNotFoundModule
+    PageNotFoundModule,
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    EffectsModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
