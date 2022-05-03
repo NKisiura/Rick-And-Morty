4f1cf85d7a39577a3e0de7722e9ab6be62563fc6
Nikita Kisiura <nikitar156@gmail.com>
2022-05-03 16:13:53 +0300

    styles pagination

--- src/app/global/modules/feature-components/pagination/components/pagination.component.ts
+++ src/app/global/modules/feature-components/pagination/components/pagination.component.ts
@@ -1,12 +1,18 @@
 import {Component, Input} from "@angular/core";
 import {Router} from "@angular/router";
 import {ParsedUrl, parseUrl} from "query-string";
+import {faAngleDoubleLeft, faAngleDoubleRight, faAngleLeft, faAngleRight} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-pagination',
   templateUrl: './pagination.component.html'
 })
 export class PaginationComponent {
+  public iconArrowLeft = faAngleLeft;
+  public iconArrowLeftDouble = faAngleDoubleLeft;
+  public iconArrowRight = faAngleRight;
+  public iconArrowRightDouble = faAngleDoubleRight;
+
   @Input() public currentPage!: number;
   @Input() public pageCount!: number;
 
--- src/app/global/modules/feature-components/pagination/pagination.module.ts
+++ src/app/global/modules/feature-components/pagination/pagination.module.ts
@@ -2,10 +2,11 @@ import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {PaginationComponent} from "./components/pagination.component";
 import {RouterModule} from "@angular/router";
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 
 @NgModule({
   declarations: [PaginationComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FontAwesomeModule],
   providers: [],
   exports: [PaginationComponent]
 })
